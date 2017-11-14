c88bcd8505a1cff9d3e1b0245cfbe306598674c9
Rafael Taveras <rafaelvtaveras@gmail.com>
2017-11-14 15:56:35 -0400

    actualizacion

--- app/src/main/java/com/example/owner/myapplication/MainActivity.java
+++ app/src/main/java/com/example/owner/myapplication/MainActivity.java
@@ -17,13 +17,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void accion(View view) {
-        EditText Texto =(EditText) findViewById(R.id.txtnombre);
-        Toast.makeText(this, "Saludos:"+Texto.getText(), Toast.LENGTH_SHORT).show();
+        EditText texto =(EditText) findViewById(R.id.txtNombre);
+        Toast.makeText(this, "Saludos:"+texto.getText(), Toast.LENGTH_SHORT).show();
 
         Intent intent=new Intent(this,Visualizar.class);
-        Bundle bundle= new Bundle();
-        bundle.putString("Nombre",Texto.getText().toString());
-        intent.putExtras(bundle);
+        intent.putExtra("nombre", texto.getText());
         startActivity(intent);
 
 
